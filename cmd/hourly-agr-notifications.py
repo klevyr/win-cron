34d77913b6f2c1bedb5cb9f06edb807ccc9a0473
klevyr <klever.ramon@gmail.com>
2026-03-02 13:24:54 -0500

    [UPD] Hourly success job

--- cmd/hourly-agr-notifications.py
+++ cmd/hourly-agr-notifications.py
@@ -3,14 +3,26 @@
 # License: AGPLv3
 from radagast import Radagast
 from datetime import date
+import pandas as pd
+
+transfer_columns = ['Fecha', 'MesID', 'DiaID', 'uid', 'Hora', 'Cola',
+                    'Response','StatusContact','Envios']
+
+def getTextQueueType(col):
+    if col == 'COLPUSHJOU':
+        return 'PUSH_SMS'
+    elif col == 'COLPUSHSMS':
+        return 'PUSH'
+    return 'SMS'
 
 if __name__ == '__main__':
     rad = Radagast()
     rad.set_logger(__file__ + ".log")
     rad.log.info("***** NEW *****")
-    dt = date.today().strftime("%Y%m%d")
+    today_dtformat = date.today().strftime("%Y-%m-%d")
+    today_asformat = date.today().strftime("%Y%m%d")
     rad.log.info("* config transfers")
-    rad.log.info(f" >> now `{dt}`")
+    rad.log.info(f" >> now `{today_asformat}`")
     # init transfers
     rad.load_config()
     rad.log.info("* init transfer config")
@@ -20,20 +32,36 @@ if __name__ == '__main__':
         filetransfer="EMAIL_AGR_NOTIF.dtfx",
         configsection="SQL",
         configkey="Where",
-        configvalue=f"E1Z141Q2 = '{dt}'"
+        configvalue=f"E1Z141Q2 = '{today_asformat}'"
     )
     rad.acsbundle_download()
     rad.set_config_transfer(
         filetransfer="SMS_AGR_NOTIF.dtfx",
         configsection="SQL",
         configkey="Where",
-        configvalue=f"S1Z141Q2 = '{dt}'"
+        configvalue=f"S1Z141Q2 = '{today_asformat}'"
     )
     rad.acsbundle_download()
     ## Otros Payclub, cna
     #rad.transfer_data("CNAPUAP_DAILY")
     #rad.transfer_data("CNACLIP_DAILY")
-    rad.log.info("* init SQL")
+    rad.log.info("* init DTL")
+    df = pd.read_csv("D:/com/share/sms_agr_notifications.csv",
+                     encoding="ISO-8859-1",
+                     names=transfer_columns,
+                     dtype=str
+                     )
+    df['TipoCola'] = df['Cola'].map(getTextQueueType)
+    rad.runSqlQuery(f"DELETE FROM `mon_notif_sms` WHERE Fecha = '{today_dtformat}'")
+    df.to_sql(name='mon_notif_sms', con=rad.getMSqlEngine(), if_exists='append', index=False)
+    df = pd.read_csv("D:/com/share/mail_agr_notifications.csv",
+                     encoding="ISO-8859-1",
+                     names=transfer_columns,
+                     dtype=str
+                     )
+    df['TipoCola'] = 'POSTFIX'
+    rad.runSqlQuery(f"DELETE FROM `mon_notif_email` WHERE Fecha = '{today_dtformat}'")
+    df.to_sql(name='mon_notif_email', con=rad.getMSqlEngine(), if_exists='append', index=False)
     #rad.run_mysql("hourly_notifications.sql")
     #rad.run_mysql("hourly_othersdb.sql")
     rad.log.info("*** end sync")
