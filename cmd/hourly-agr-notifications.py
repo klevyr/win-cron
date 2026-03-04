2c7ccc2fd645777127e7788a840032d97ea8945e
klevyr <klever.ramon@gmail.com>
2026-03-04 12:20:30 -0500

    [UPD] others DBs

--- cmd/hourly-agr-notifications.py
+++ cmd/hourly-agr-notifications.py
@@ -43,10 +43,12 @@ if __name__ == '__main__':
     )
     rad.acsbundle_download()
     ## Otros Payclub, cna
-    #rad.transfer_data("CNAPUAP_DAILY")
-    #rad.transfer_data("CNACLIP_DAILY")
+    rad.set_current_transfer_filename("CNAPUAP_DAILY.dtfx").acsbundle_download()
+    rad.set_current_transfer_filename("CNACLIP_DAILY.dtfx").acsbundle_download()
+    # init uploads
+    # SMS
     rad.log.info("* init DTL")
-    df = pd.read_csv("D:/com/share/sms_agr_notifications.csv",
+    df = pd.read_csv(f"{rad.defaultOutputData}/sms_agr_notifications.csv",
                      encoding="ISO-8859-1",
                      names=transfer_columns,
                      dtype=str
@@ -54,7 +56,9 @@ if __name__ == '__main__':
     df['TipoCola'] = df['Cola'].map(getTextQueueType)
     rad.runSqlQuery(f"DELETE FROM `mon_notif_sms` WHERE Fecha = '{today_dtformat}'")
     df.to_sql(name='mon_notif_sms', con=rad.getMSqlEngine(), if_exists='append', index=False)
-    df = pd.read_csv("D:/com/share/mail_agr_notifications.csv",
+    rad.log.info("> SMS, done.")
+    # EMAIL
+    df = pd.read_csv(f"{rad.defaultOutputData}/mail_agr_notifications.csv",
                      encoding="ISO-8859-1",
                      names=transfer_columns,
                      dtype=str
@@ -62,15 +66,33 @@ if __name__ == '__main__':
     df['TipoCola'] = 'POSTFIX'
     rad.runSqlQuery(f"DELETE FROM `mon_notif_email` WHERE Fecha = '{today_dtformat}'")
     df.to_sql(name='mon_notif_email', con=rad.getMSqlEngine(), if_exists='append', index=False)
-    #rad.run_mysql("hourly_notifications.sql")
-    #rad.run_mysql("hourly_othersdb.sql")
+    rad.log.info("> EMAIL, done.")
+    # CNACLIP
+    df = pd.read_csv(f"{rad.defaultOutputData}/cnaclip-daily.csv",
+                     encoding="ISO-8859-1",
+                     names=['cldocu','cltipp','cltise','clfevc','clusbi'],
+                     dtype=str
+                     )
+    rad.runSqlQuery(f"TRUNCATE TABLE `cna_cnaclip`")
+    df.to_sql(name='cna_cnaclip', con=rad.getMSqlEngine(), if_exists='append', index=False)
+    rad.log.info("> CNACLIP, done.")
+    # CNAPUAP
+    df = pd.read_csv(f"{rad.defaultOutputData}/cnapuap-daily.csv",
+                     encoding="ISO-8859-1",
+                     names=['ruc','tipp','cedsoc','usname','email','celnum','profile','passwd',
+                            'usbitmp','sendate','sendtime','usbidef','defsendate','defsendtime'],
+                     dtype=str
+                     )
+    rad.runSqlQuery(f"TRUNCATE TABLE `cna_cnapuap`")
+    df.to_sql(name='cna_cnapuap', con=rad.getMSqlEngine(), if_exists='append', index=False)
+    rad.log.info("> CNAPUAP, done.")
+
     rad.log.info("*** end sync")
     files_remove = [
-        "D:/com/share/sms_notifications.csv",
-        "D:/com/share/mail_notifications.csv",
-        "D:/com/share/bop_trans.csv",
-        "D:/com/share/cnapuap-daily.csv",
-        "D:/com/share/cnaclip-daily.csv",
+        f"{rad.defaultOutputData}/sms_agr_notifications.csv",
+        f"{rad.defaultOutputData}/mail_agr_notifications.csv",
+        f"{rad.defaultOutputData}/cnapuap-daily.csv",
+        f"{rad.defaultOutputData}/cnaclip-daily.csv",
     ]
-    #rad.clean_transfer_data(files_remove)
+    rad.clean_transfer_data(files_remove)
     rad.log.info("done!")
